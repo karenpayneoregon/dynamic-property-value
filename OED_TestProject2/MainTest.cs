8cfc4bcf2f20f259b007e039aa58e91b8ab2a513
Karen Payne <Karen.1.Payne@oregon.gov>
2021-09-30 17:34:09 -0700

    blank test project

---
+++ OED_TestProject2/MainTest.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OED_TestProject2.Base;
+
+namespace OED_TestProject2
+{
+    [TestClass]
+    public partial class MainTest : TestBase
+    {
+        [TestMethod]
+        [TestTraits(Trait.PlaceHolder)]
+        public void TestMethod1()
+        {
+            // arrange
+
+
+            // act
+
+
+            // assert
+        }
+        [TestMethod]
+        [TestTraits(Trait.PlaceHolder)]
+        public void TestMethod2()
+        {
+            // TODO
+        }
+    }
+}
